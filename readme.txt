47415edfff8f7cd15217437b7efdb20f752c332a
Muhammad_Haris_Ali <haris@justads.com>
2022-04-05 23:03:50 +0500

    test cases added

--- readme.txt
+++ readme.txt
@@ -3,4 +3,9 @@
 #command to run
  python exercise.py
 
+# command to install pytest
+ pip install pytest
+
+#command to run tests
+ pytest test.py -v
 
--- test.py
+++ test.py
@@ -1,17 +1,35 @@
 from pathlib import Path
+import exercise
+
+
 def test_user_file_exist():
-    path_to_file = '/Users/muhammadharis/Desktop/coding-exercise/users.json'
+    path_to_file = './users.json'
     path = Path(path_to_file)
     assert True == path.is_file()
 
 
 def test_ticket_file_exist():
-    path_to_file = '/Users/muhammadharis/Desktop/coding-exercise/tickets.json'
+    path_to_file = './tickets.json'
     path = Path(path_to_file)
     assert True == path.is_file()
 
 
 def test_organization_file_exist():
-    path_to_file = '/Users/muhammadharis/Desktop/coding-exercise/organizations.json'
+    path_to_file = './organizations.json'
     path = Path(path_to_file)
     assert True == path.is_file()
+
+
+def test_constructor_user_data_object():
+    obj = exercise.Search()
+    assert True == bool(obj.data_user)
+
+
+def test_constructor_data_ticket_object():
+    obj = exercise.Search()
+    assert True == bool(obj.data_ticket)
+
+
+def test_constructor_data_organization_object():
+    obj = exercise.Search()
+    assert True == bool(obj.data_organization)
